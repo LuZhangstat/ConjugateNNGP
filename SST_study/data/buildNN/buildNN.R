ed3e404c41d301614cbeca6b3f6b45b96e073577
Lu Zhang <lu.zhang@ucla.edu>
2018-06-11 19:37:43 -0500

    move NNMatrix to src folder

--- SST_study/data/buildNN/buildNN.R
+++ SST_study/data/buildNN/buildNN.R
@@ -9,7 +9,6 @@ load("data/data/realdata.RData")
 M = 10
 library(spNNGP)
 sigma.sq.IG <- c(as, bs)
-cov.model <- "exponential"
 theta.alpha <- c((ap + bp) / 2, bt / bs)
 names(theta.alpha) <- c("phi", "alpha")
 
@@ -19,21 +18,7 @@ NN.matrix <- NNMatrix(coords = coords, n.neighbors = M,
 save(list = ls(all.names = TRUE), file = "data/buildNN/nngp_10.RData", envir = .GlobalEnv)
 
 
-#par(mfrow = c(1, 1))
-#for(i in 200:220){
-#  plot(m.c$coords.ord[1:2000,]) # all points
-#  points(m.c$coords.ord[1:i, , drop = FALSE], col = "grey", pch = 19)
-  
-  # neighbors
-#  if (i < M) {dim = i} else {dim = M}
-#  for (j in 1:dim){
-#    points(m.c$coords.ord[NN.matrix$nearind[i - 1, j], , drop = FALSE],
-#           col = "orange",  pch = 19)
-#  }
-#  points(m.c$coords.ord[i, , drop = FALSE], col = "blue", pch = 19) # the target point
-  
-#  readline(prompt = "Pause. Press <Enter> to continue...")
-#}
+
 
 
 
